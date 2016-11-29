52887b8eccf731068f7c407612ae737489432919
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-11-29 14:32:06 -0800

    Update package.json & requirements.txt

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,39 @@ const config = {
     path: path.resolve(__dirname, 'public/build'),
     filename: 'bundle.js'
   },
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query:
+        {
+          presets: ['es2015', 'react', 'stage-2'],
+          compact: false
+        }
+      },
+      { test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query:
+        {
+          presets: ['es2015', 'react', 'stage-2'],
+          compact: false
+        }
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /node_modules/, loader: 'ify' }
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   }
