b792162e68b4f4bfc00970df1f45a08b7a559119
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-12-02 13:16:03 -0800

    Add working tooltips tests

--- survey_app/tests/frontend/test_tooltips.py
+++ survey_app/tests/frontend/test_tooltips.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-from cesium_app.tests.fixtures import create_test_project
+from survey_app.tests.fixtures import create_test_project
 
 def test_tab_tooltips(driver):
     driver.get('/')
@@ -22,7 +22,7 @@ def test_tab_tooltips(driver):
         hover.perform()
         time.sleep(1)
         assert driver.find_element_by_xpath(
-            "//span[contains(text(),'Upload your time-series data')]"
+            "//span[contains(text(),'Upload your light curves')]"
             ).is_displayed()
 
         hover = ActionChains(driver).move_to_element(
@@ -30,7 +30,7 @@ def test_tab_tooltips(driver):
         hover.perform()
         time.sleep(1)
         assert driver.find_element_by_xpath(
-            "//span[contains(text(),'Generate features from your time-series data')]"
+            "//span[contains(text(),'Generate predictions for new data')]"
         ).is_displayed()
 
 
