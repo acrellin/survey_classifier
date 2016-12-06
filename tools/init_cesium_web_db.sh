880aeb49dfc495b68a0da4d232d0a85b499a712a
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-12-06 10:35:43 -0800

    Add script to copy & run survey_db_init in cesium_web

---
+++ tools/init_cesium_web_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+APP_REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp ./tools/survey_db_init.py ../cesium_web
+cd ../cesium_web
+make paths
+make db_init
+python survey_db_init.py
+rm survey_db_init.py
+make debug &
+cd $APP_REPO_DIR
