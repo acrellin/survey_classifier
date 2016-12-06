627fd751bad8da8962b21bed5f4c7eb983b0db7f
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-12-05 17:34:04 -0800

    Initialize cesium_web database in Travis script

--- .drone/travis_install.sh
+++ .drone/travis_install.sh
@@ -50,6 +50,7 @@ git clone https://github.com/cesium-ml/cesium_web.git
 cd cesium_web
 cp ../tools/survey_db_init.py ./
 make paths
+make db_init
 pip install --retries 3 -r requirements.txt
 python survey_db_init.py
 make &
