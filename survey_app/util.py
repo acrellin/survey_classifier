626196e2fd8ff1e22a8f1d974546836e2a5de79b
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-01-04 10:37:38 -0800

    Add utility function to group prediction results by TS name; normalize probs

--- survey_app/util.py
+++ survey_app/util.py
@@ -4,6 +4,7 @@ import ast
 import hashlib
 import csv
 import requests
+from collections import defaultdict
 from .config import cfg
 
 
@@ -97,10 +98,44 @@ def determine_model_ids(prediction_results):
                         requests.get('{}/models'.format(cfg['cesium_app']['url']))
                         .json()['data'] if model['project'] ==
                         cfg['cesium_app']['survey_classifier_project_id']}
-    print(model_name_to_id)
-    ts_name_model_ids = {}
+    ts_name_model_ids_and_probs = {}
     for ts_name in prediction_results:
-        ts_name_model_ids[ts_name] = [
-            model_name_to_id[model_name] for model_name, prob in
-            prediction_results[ts_name]['prediction'].items() if prob >= 0.05]
-    return ts_name_model_ids
+        ts_name_model_ids_and_probs[ts_name] = {
+            model_name_to_id[model_name]: prob for model_name, prob in
+            prediction_results[ts_name]['prediction'].items() if prob >= 0.05}
+    # Normalize probabilities
+    for ts_name in ts_name_model_ids_and_probs:
+        old_sum = sum(ts_name_model_ids_and_probs[ts_name].values())
+        ts_name_model_ids_and_probs[ts_name] = {
+            model_id: old_prob / old_sum
+            for model_id, old_prob in ts_name_model_ids_and_probs[ts_name].items()}
+    return ts_name_model_ids_and_probs
+
+
+def aggregate_pred_results_by_ts(sci_pred_results, science_model_ids_and_probs):
+    """Map model-wise prediction results to TS-wise results.
+
+    Parameters
+    ----------
+    sci_pred_results : dict
+        Dictionary whose keys are cesium_web model IDs, and values are
+        dictionaries containing prediction results for that model.
+    science_model_ids_and_probs : dict
+        BLAH
+
+    Returns
+    -------
+    pred_results_by_ts : dict
+        Dictionary whose keys are TS names and values are weighted prediction
+        results
+    """
+    ts_names = set([ts_name for model_id in sci_pred_results
+                    for ts_name in sci_pred_results[model_id]])
+    pred_results_by_ts = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
+    for model_id, results_dict in sci_pred_results.items():
+        for ts_name, ts_results in results_dict.items():
+            pred_results_by_ts[ts_name]['by_model'][model_id] = ts_results['prediction']
+            for sci_class, prob in ts_results['prediction'].items():
+                pred_results_by_ts[ts_name]['combined'][sci_class] += (
+                    science_model_ids_and_probs[ts_name][model_id] * prob)
+    return pred_results_by_ts
