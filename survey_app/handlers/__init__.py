f80fb0ded8c3d8c311fa379b6e79d55163709b12
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-01-05 16:25:20 -0800

    Update handlers/__init__.py

--- survey_app/handlers/__init__.py
+++ survey_app/handlers/__init__.py
@@ -2,5 +2,6 @@ from .base import BaseHandler, AccessError
 from .socket_auth import SocketAuthTokenHandler
 from .project import ProjectHandler
 from .dataset import DatasetHandler
-from .prediction import SurveyPredictionHandler, SciencePredictionHandler
+from .survey_prediction import SurveyPredictionHandler
+from .science_prediction import SciencePredictionHandler
 from .model import ModelHandler
