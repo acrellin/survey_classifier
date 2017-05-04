57abfd7e7d7aec04a4c4207c6ba798a8de0a1715
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-05-04 15:19:09 -0700

    Revert webpack config to previous version

--- webpack.config.js
+++ webpack.config.js
@@ -8,47 +8,40 @@ const config = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [
+    loaders: [
       { test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options:
-                {
-                  presets: ['es2015', 'react', 'stage-2'],
-                  compact: false
-                }
+        loader: 'babel',
+        query:
+              {
+                presets: ['es2015', 'react', 'stage-2'],
+                compact: false
+              }
       },
       { test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options:
-                {
-                  presets: ['es2015', 'react', 'stage-2'],
-                  compact: false
-                }
+        loader: 'babel',
+        query:
+              {
+                presets: ['es2015', 'react', 'stage-2'],
+                compact: false
+              }
       },
-      { test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /node_modules/, loader: 'ify' }
     ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     }),
-
-    // We do not use JQuery for anything in this project; but Bootstrap
-    // depends on it
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
-    })
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['', '.js', '.jsx']
   }
 };
 
