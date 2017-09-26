d63a813be028f7c94eced4f24845ecf45d356a6a
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-09-26 11:29:23 -0700

    Update general_prediction handler to reflect new DB; add auth to endpoints

--- survey_app/handlers/general_prediction.py
+++ survey_app/handlers/general_prediction.py
@@ -3,37 +3,29 @@ from baselayer.app.custom_exceptions import AccessError
 from ..models import Prediction, Project, DBSession
 
 import requests
+import tornado.web
 
 
 class GeneralPredictionHandler(BaseHandler):
     """Handler for fetching and deleting predictions."""
-    def _get_prediction(self, prediction_id):
-        try:
-            d = Prediction.get(Prediction.id == prediction_id)
-        except Prediction.DoesNotExist:
-            raise AccessError('No such dataset')
-
-        if not d.is_owned_by(self.get_username()):
-            raise AccessError('No such dataset')
-
-        return d
-
+    @tornado.web.authenticated
     def get(self, prediction_id=None):
         if prediction_id is None:
             predictions = [prediction
-                           for project in Project.all(self.get_username())
+                           for project in self.current_user.projects
                            for prediction in project.predictions]
             prediction_info = [p.display_info() for p in predictions]
         else:
-            prediction = self._get_prediction(prediction_id)
+            prediction = Prediction.get_if_owned_by(prediction_id,
+                                                    self.current_user)
             prediction_info = prediction.display_info()
 
-        return self.success(prediction_info)
+            return self.success(prediction_info)
 
+    @tornado.web.authenticated
     def delete(self, prediction_id):
-        prediction = self._get_prediction(prediction_id)
-        # Make request to delete prediction in cesium_web
-        r = requests.delete('{}/predictions/{}'.format(
-            self.cfg['cesium_app:url'], prediction.cesium_app_id)).json()
-        prediction.delete_instance()
+        prediction = Prediction.get_if_owned_by(prediction_id,
+                                                self.current_user)
+        DBSession().delete(prediction)
+        DBSession().commit()
         return self.success(action='survey_app/FETCH_PREDICTIONS')
