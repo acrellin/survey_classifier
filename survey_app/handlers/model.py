a25d036f80138eb4956dceadb5366fede2f4bd69
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-09-26 11:29:49 -0700

    Update model handler to reflect new DB; add auth to endpoints

--- survey_app/handlers/model.py
+++ survey_app/handlers/model.py
@@ -1,19 +1,21 @@
 '''Handler for '/models' route.'''
 
-from .base import BaseHandler
-from ..config import cfg
+from baselayer.app.handlers.base import BaseHandler
+from baselayer.app.custom_exceptions import AccessError
 
 import requests
+import tornado.web
 
 
 class ModelHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self, model_id=None):
         if model_id is not None:
             model_info = requests.get('{}/models/{}'.format(
-                cfg['cesium_app']['url'], model_id)).json()['data']
+                self.cfg['cesium_app:url'], model_id)).json()['data']
         else:
             model_info = [model for model in requests.get('{}/models'.format(
-                cfg['cesium_app']['url'])).json()['data'] if model['project_id'] ==
-                          cfg['cesium_app']['survey_classifier_project_id']]
+                self.cfg['cesium_app:url'])).json()['data'] if model['project_id'] ==
+                          self.cfg['cesium_app:survey_classifier_project_id']]
 
         return self.success(model_info)
