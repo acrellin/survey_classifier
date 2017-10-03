9bad6954ed8ddcf2c66e7b82c23efab6e31f2485
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-10-03 10:42:46 -0700

    Change socket path in baselayer.app.flow

--- baselayer/app/flow.py
+++ baselayer/app/flow.py
@@ -6,7 +6,7 @@ class Flow(object):
     """Send messages through websocket to frontend
 
     """
-    def __init__(self, socket_path='ipc:///tmp/message_flow_in'):
+    def __init__(self, socket_path='ipc:///tmp/message_flow_in_sa'):
         self._socket_path = socket_path
         self._ctx = zmq.Context.instance()
         self._bus = self._ctx.socket(zmq.PUSH)
