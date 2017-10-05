8b7d43da955551146ee931289cc61499a43635f7
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-10-05 16:08:54 -0700

    Change PYTHONPATH in survey_db_init script

--- tools/survey_db_init.sh
+++ tools/survey_db_init.sh
@@ -5,7 +5,7 @@ cp ./tools/survey_db_init.py ../cesium_web
 cd ../cesium_web
 make paths
 make db_init
-python survey_db_init.py
+PYTHONPATH=.. python survey_db_init.py
 rm survey_db_init.py
 make log &
 make &
