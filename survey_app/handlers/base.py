e7ca7b850313b7a1b1eb6b78dc4f0fd25935d82b
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-10-12 18:39:38 -0700

    Add execution of token generation script to DB init script

--- survey_app/handlers/base.py
+++ survey_app/handlers/base.py
@@ -1,3 +1,5 @@
+import os
+
 from baselayer.app.handlers.base import BaseHandler as BLBaseHandler
 
 
@@ -5,6 +7,6 @@ class BaseHandler(BLBaseHandler):
     def get_cesium_auth_cookie(self):
         if not hasattr(self, 'cesium_auth_cookie'):
             with open(os.path.join(self.cfg['paths:cesium_web_login_cookie_folder'],
-                                   '/cesium_web_token')) as f:
+                                   'cesium_web_token')) as f:
                 self.cesium_auth_cookie = {'token': f.read().strip()}
         return self.cesium_auth_cookie
--- tools/survey_db_init.sh
+++ tools/survey_db_init.sh
@@ -2,12 +2,15 @@
 
 APP_REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp ./tools/survey_db_init.py ../cesium_web
+cp ./tools/generate_token.py ../cesium_web/tools
 cd ../cesium_web
 make paths
 make dependencies
 make db_init
 PYTHONPATH=.. python survey_db_init.py
+PYTHONPATH=. python tools/generate_token.py survey_app_bot
 rm survey_db_init.py
+rm tools/generate_token.py
 make log &
 make &
 sleep 3
