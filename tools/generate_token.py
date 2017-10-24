1ce5b4fb82d72d839ee155724590e7d6446c88cc
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-10-24 16:55:41 -0700

    Update generate_token

--- tools/generate_token.py
+++ tools/generate_token.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 
 import argparse
+import uuid
+import jwt
 import os
 
 from baselayer.app.config import load_config
-from cesium_app.model_util import create_token_user
+from baselayer.app.models import init_db
+from cesium_app import model_util
 
+cfg_paths = [os.path.join(os.path.dirname(__file__),
+                          '../../survey_app/config.yaml.example'),
+             os.path.join(os.path.dirname(__file__),
+                          '../config.yaml.example')]
 
-cfg = load_config([os.path.join(os.path.dirname(__file__),
-                                '../../survey_app/config.yaml.example'),
-                   os.path.join(os.path.dirname(__file__),
-                                '../config.yaml.example')])
+cfg = load_config(cfg_paths)
+
+conn = init_db(**cfg['database'])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('bot_name')
+
     args = parser.parse_args()
 
-    token = create_token_user(args.bot_name,
-                              cfg['cesium_app:survey_classifier_project_id'])
+    token = model_util.create_token_user(args.bot_name,
+                                         [cfg['cesium_app:cesium_app_project_id']])
 
     with open((os.path.abspath(os.path.join(
             '../survey_app/', cfg['paths:cesium_web_login_token_folder'],
