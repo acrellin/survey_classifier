c21b82a6b3ce38cfb0bc3f50cdaa54d931299f64
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-10-25 13:55:06 -0700

    Use full path to cesium auth token in config

--- survey_app/handlers/base.py
+++ survey_app/handlers/base.py
@@ -6,7 +6,6 @@ from baselayer.app.handlers.base import BaseHandler as BLBaseHandler
 class BaseHandler(BLBaseHandler):
     def get_cesium_auth_token(self):
         if not hasattr(self, 'cesium_auth_token'):
-            with open(os.path.join(self.cfg['paths:cesium_web_login_token_folder'],
-                                   'cesium_web_token')) as f:
+            with open(self.cfg['paths:cesium_web_login_token_path']) as f:
                 self.cesium_auth_token = f.read().strip()
         return self.cesium_auth_token
--- tools/generate_token.py
+++ tools/generate_token.py
@@ -10,9 +10,9 @@ from baselayer.app.models import init_db
 from cesium_app import model_util
 
 cfg_paths = [os.path.join(os.path.dirname(__file__),
-                          '../../survey_app/config.yaml.defaults'),
+                          '../config.yaml.defaults'),
              os.path.join(os.path.dirname(__file__),
-                          '../config.yaml.defaults')]
+                          '../../cesium_web/config.yaml.defaults')]
 
 cfg = load_config(cfg_paths)
 
@@ -28,9 +28,7 @@ if __name__ == '__main__':
     token = model_util.create_token_user(
         args.bot_name, [cfg['cesium_app:survey_classifier_project_id']])
 
-    with open((os.path.abspath(os.path.join(
-            '../survey_app/', cfg['paths:cesium_web_login_token_folder'],
-            'cesium_web_token'))), 'w') as f:
+    with open(cfg['paths:cesium_web_login_token_path'], 'w') as f:
         f.write(token)
 
     print('Successfully created token user and stored token to disk')
